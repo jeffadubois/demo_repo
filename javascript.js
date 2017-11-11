9bbd6fe16bbbbf7ae502f1076fdc2bee9fe53f91
Jeff DuBois <jeff.adubois@gmail.com>
2017-11-11 08:54:03 -0600

    updates for collapseable functionality

---
+++ javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){ 
+
+   document.getElementById("expandContentid").addEventListener("click", function(){
+   document.getElementById("collapseid").style = "display:none";
+   });
+});
+
+//   $('.expandContent').click(function(){
+//       $('.collapseclass').toggle();
+//    });
+
+//});
