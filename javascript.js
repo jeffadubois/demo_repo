cb26d38ebf86b025a2bf89c8ba998c04dbea3592
Jeff DuBois <jeff.adubois@gmail.com>
2017-11-11 23:10:49 -0600

    fix for collapseable bio information

--- javascript.js
+++ javascript.js
@@ -1,12 +1,40 @@
 $(document).ready(function(){ 
 
-   document.getElementById("expandContentid").addEventListener("click", function(){
-   document.getElementById("collapseid").style = "display:none";
+   $('#lessid').hide();
+   $('#collapseid').hide();
+   document.getElementById("moreid").addEventListener("click", function(){
+   	     $('#collapseid').toggle();
+   	     if($('#collapseid').is(':visible')){
+   	     	$('#lessid').show();
+   	     	$('#moreid').hide();
+   	     	//$('#expandContentid').text('Less...<a href="#">Less...</a>');
+   	     } else {
+    	   	$('#lessid').hide();
+   	     	$('#moreid').show();
+  	     	
+   	     	//$('#expandContentid').text('More...<a href="#">More...</a>');
+   	     }
+   	});
+   document.getElementById("lessid").addEventListener("click", function(){
+   	     $('#collapseid').toggle();
+   	     if($('#collapseid').is(':visible')){
+   	     	$('#lessid').show();
+   	     	$('#moreid').hide();
+   	     	//$('#expandContentid').text('Less...<a href="#">Less...</a>');
+   	     } else {
+    	   	$('#lessid').hide();
+   	     	$('#moreid').show();
+  	     	
+   	     	//$('#expandContentid').text('More...<a href="#">More...</a>');
+   	     }
+
+   //document.getElementById("collapseid").style = "display:none";
    });
-});
-
-//   $('.expandContent').click(function(){
-//       $('.collapseclass').toggle();
-//    });
-
 //});
+
+   $('.expandContent').click(function(){
+       $('p').toggleClass(".collapseclass");
+	   console.log("got here");
+    });
+
+});
